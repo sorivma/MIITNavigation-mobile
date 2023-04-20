0f5a36b01aa186b918607898652ef4c1e64bbcf2
lrnvkrt <krist1na.larion0va@yandex.ru>
2023-04-20 23:47:35 +0300

    Edit the News and Timetable.

--- app/src/main/java/com/example/miit_application/MainActivity.java
+++ app/src/main/java/com/example/miit_application/MainActivity.java
@@ -17,9 +17,9 @@ public class MainActivity extends AppCompatActivity {
 
     ActivityMainBinding binding;
 
-    private TextView textView;
-    private Handler handler = new Handler();
-    private int[][] intervals = {{8, 30, 9, 50}, {10, 5, 11, 25}, {11, 40, 13, 0}, {13, 45, 15, 5}, {15, 20, 16, 40}, {16, 55, 18, 15}, {18, 30, 19, 50}, {20, 5, 21, 25}};
+//    private TextView textView;
+//    private Handler handler = new Handler();
+//    private int[][] intervals = {{8, 30, 9, 50}, {10, 5, 11, 25}, {11, 40, 13, 0}, {13, 45, 15, 5}, {15, 20, 16, 40}, {16, 55, 18, 15}, {18, 30, 19, 50}, {20, 5, 21, 25}};
 
 
     @Override
@@ -55,88 +55,59 @@ public class MainActivity extends AppCompatActivity {
             return true;
         });
 
-        textView = findViewById(R.id.textView2);
-
-        updateTime();
+//        textView = findViewById(R.id.textView2);
+//
+//        updateTime();
     }
 
-    private void updateTime() {
-//        Calendar calendar = Calendar.getInstance();
-//        int currentHour = calendar.get(Calendar.HOUR_OF_DAY);
-//        int currentMinute = calendar.get(Calendar.MINUTE);
+//    private void updateTime() {
 //
-        LocalTime currentTime = LocalTime.now();
-
-        String text = "";
-
+//        LocalTime currentTime = LocalTime.now();
+//
+//        String text = "";
+//
 //        for (int i = 0; i < intervals.length; i++) {
 //            int startHour = intervals[i][0];
 //            int startMinute = intervals[i][1];
 //            int endHour = intervals[i][2];
 //            int endMinute = intervals[i][3];
 //
+//            LocalTime startTime = LocalTime.of(startHour, startMinute);
+//            LocalTime endTime = LocalTime.of(endHour, endMinute);
 //
-//                if ((startHour==currentHour && currentMinute >= startMinute) || (currentHour==endHour && currentMinute < endMinute) || (currentHour > startHour && currentHour < endHour)) {
-//                    text = startHour + ":" + String.format("%02d", startMinute) + " - " + endHour + ":" + String.format("%02d", endMinute) + " - " + (i + 1) + " пара";
-//                    break;
-//                } else if (currentHour==endHour && currentMinute >= endMinute || (intervals[i+1][1]>currentMinute && intervals[i+1][0]==currentHour)) {
-//                    text = endHour + ":" + String.format("%02d", endMinute) + " - " + intervals[i+1][0] + ":" + String.format("%02d", intervals[i+1][1]) + " - " + "перерыв";
-//                    break;
-//                } else if (intervals[i+1][0]==currentHour && currentMinute < intervals[i+1][1]) {
-//                    text = endHour + ":" + String.format("%02d", endMinute) + " - " + intervals[i+1][0] + ":" + String.format("%02d", intervals[i+1][1]) + " - " + "перерыв";
-//                    break;
-//                }
-//
-//                if ((currentHour < intervals[0][0] && currentMinute < intervals[0][1]) || (currentHour > intervals[7][0] && currentMinute > intervals[7][1])) {
+//            if (currentTime.isAfter(startTime) && currentTime.isBefore(endTime)) {
+//                text = startTime + " - " + endTime + " - " + (i + 1) + " пара";
+//                break;
+//            } else if (currentTime.equals(endTime)) {
+//                if (i == intervals.length - 1) {
 //                    text = "Занятий нет";
-//                    break;
+//                } else {
+//                    int nextStartHour = intervals[i+1][0];
+//                    int nextStartMinute = intervals[i+1][1];
+//                    LocalTime nextStartTime = LocalTime.of(nextStartHour, nextStartMinute);
+//                    text = endTime + " - " + nextStartTime + " - " + "перерыв";
 //                }
-//
-//
+//                break;
+//            } else if (currentTime.isBefore(startTime)) {
+//                if (i == 0) {
+//                    text = "Занятий нет";
+//                } else {
+//                    int prevEndHour = intervals[i-1][2];
+//                    int prevEndMinute = intervals[i-1][3];
+//                    LocalTime prevEndTime = LocalTime.of(prevEndHour, prevEndMinute);
+//                    text = prevEndTime + " - " + startTime + " - " + "перерыв";
+//                }
+//                break;
+//            } else if (i == intervals.length - 1) {
+//                text = "Занятий нет";
+//                break;
+//            }
 //        }
-
-        for (int i = 0; i < intervals.length; i++) {
-            int startHour = intervals[i][0];
-            int startMinute = intervals[i][1];
-            int endHour = intervals[i][2];
-            int endMinute = intervals[i][3];
-
-            LocalTime startTime = LocalTime.of(startHour, startMinute);
-            LocalTime endTime = LocalTime.of(endHour, endMinute);
-
-            if (currentTime.isAfter(startTime) && currentTime.isBefore(endTime)) {
-                text = startTime + " - " + endTime + " - " + (i + 1) + " пара";
-                break;
-            } else if (currentTime.equals(endTime)) {
-                if (i == intervals.length - 1) {
-                    text = "Занятий нет";
-                } else {
-                    int nextStartHour = intervals[i+1][0];
-                    int nextStartMinute = intervals[i+1][1];
-                    LocalTime nextStartTime = LocalTime.of(nextStartHour, nextStartMinute);
-                    text = endTime + " - " + nextStartTime + " - " + "перерыв";
-                }
-                break;
-            } else if (currentTime.isBefore(startTime)) {
-                if (i == 0) {
-                    text = "Занятий нет";
-                } else {
-                    int prevEndHour = intervals[i-1][2];
-                    int prevEndMinute = intervals[i-1][3];
-                    LocalTime prevEndTime = LocalTime.of(prevEndHour, prevEndMinute);
-                    text = prevEndTime + " - " + startTime + " - " + "перерыв";
-                }
-                break;
-            } else if (i == intervals.length - 1) {
-                text = "Занятий нет";
-                break;
-            }
-        }
-
-        textView.setText(text);
-
-        handler.postDelayed(this::updateTime, 1000); // Update every second
-    }
+//
+//        textView.setText(text);
+//
+//        handler.postDelayed(this::updateTime, 1000); // Update every second
+//    }
 
     private void replaceFragment(Fragment fragment){
         FragmentManager fragmentManager = getSupportFragmentManager();
