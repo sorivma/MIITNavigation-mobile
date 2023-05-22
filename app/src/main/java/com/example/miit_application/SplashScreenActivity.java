d0d5ab6c8bbaa6b07a305b8b1b79d8f4f375742b
lrnvkrt <krist1na.larion0va@yandex.ru>
2023-05-22 18:57:09 +0300

    Change AndroidManifest.xml. Add SplashScreenActivity.java, activity_splash_screen.xml.

---
+++ app/src/main/java/com/example/miit_application/SplashScreenActivity.java
@@ -0,0 +1,21 @@
+package com.example.miit_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(() -> {
+            startActivity(new Intent(getApplicationContext(), MainActivity.class));
+            finish();
+        }, 4000);
+    }
+}
